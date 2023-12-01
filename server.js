80be989b2f1b9fd74ebf58705ee5726c4d5c989c
andria-goodwin <143760838+andria-goodwin@users.noreply.github.com>
2023-12-01 16:40:33 -0600

    fixed routes bug

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
-const routes = require('./routes/routes.js');
+const routes = require('./routes/apiRoutes.js');
+
+const htmlRouter = require('./routes/htmlRoutes.js');
 
 const app = express();
 
@@ -10,7 +12,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
-app.use('/routes', routes);
+app.use(htmlRouter);
 
 // app listener
 app.listen(PORT, () => {
