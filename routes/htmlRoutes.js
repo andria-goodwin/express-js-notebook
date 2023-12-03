bc56f9265d309ee62642005f080f6a261791e6f6
andria-goodwin <143760838+andria-goodwin@users.noreply.github.com>
2023-12-02 18:44:07 -0600

    added api routes

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,14 +1,10 @@
 const htmlRouter = require('express').Router();
 const path = require('path');
 
-
-
-// GET /notes should return the notes.html file.
 htmlRouter.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/notes.html'));
 });
 
-// GET * should return the index.html file.
 htmlRouter.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/index.html'));
 })
