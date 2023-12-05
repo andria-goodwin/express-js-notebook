9f6d27baf7628e8cbb0359a7f8abad96f858f465
andria-goodwin <143760838+andria-goodwin@users.noreply.github.com>
2023-12-05 15:56:49 -0600

    added delete request fuctionality

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -4,14 +4,12 @@ const fs = require('fs');
 let uniqid = require('uniqid');
 
 apiRouter.get('/notes', (req, res) => {
-    console.log('get notes accessed')
     res.sendFile(path.join(__dirname, '../db/db.json'));
 });
 
 apiRouter.post('/notes', (req, res) => {
     let db = fs.readFileSync('db/db.json');
     db = JSON.parse(db);
-    // res.json(db);
 
     let userNote = {
       title: req.body.title,
@@ -22,8 +20,16 @@ apiRouter.post('/notes', (req, res) => {
     db.push(userNote);
     fs.writeFileSync('db/db.json', JSON.stringify(db));
     res.json(db);
-
-    console.log(db);
 });
+
+apiRouter.delete('/notes/:id', (req, res) => {
+  // reading notes form db.json
+  let db = JSON.parse(fs.readFileSync('db/db.json'))
+  // removing note with id
+  let deleteNotes = db.filter(item => item.id !== req.params.id);
+  // Rewriting note to db.json
+  fs.writeFileSync('db/db.json', JSON.stringify(deleteNotes));
+  res.json(deleteNotes);
+})
 
 module.exports = apiRouter;
